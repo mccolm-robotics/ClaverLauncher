2692422abd6dcdb9a56773c804e2f2bb1fc0ab5a
Harmony Lab <lead@harmonylab.com>
2020-09-23 21:15:44 -0700

    Added event trigger for updating app from git repo.

--- init.py
+++ init.py
@@ -23,12 +23,10 @@ class Init:
         self.repository_name = "ClaverNode" # Name of the git repository to load
         self.repository_class_name = self.repository_name   # Name of the entry_point class for the application
         self.repository_url = "https://github.com/mccolm-robotics/" + self.repository_name + ".git"     # Repository URL
-        self.exit_status = None    # Exit status for the application
-
+        self.action_request = None    # Exit status for the application
         if os.path.isfile("config.txt"):
             self.load_config_file("config.txt")
-            self.repository_name = self.config["dir"]
-
+            self.repository_name = self.config["app_dir"]
         # Make sure module 'psutil' is installed
         psutil_check = subprocess.run(["pip", "show", "psutil"], capture_output=True, encoding="utf-8")
         if not psutil_check.stdout:
@@ -40,52 +38,42 @@ class Init:
 
     def run(self):
         ''' Main execution area of launcher '''
-        self.activate_venv()    #
-        if self.load_client_app():
-            self.launch_app()
-        self.evaluate_exit_status()
-        # Update config with successful run status
+        self.activate_venv()    # Ensures virtual environment is installed and switches over to it.
+        if self.load_client_app():  # Ensures a version of the app has been downloaded and configured to run
+            self.launch_app()   # Instantiantes and loads app (based on repo name) and deletes previously installed versions
+        self.evaluate_action_request()  # Checks for messages sent back from the app
+        self.save_config_file()    # Saves app config-state to config.txt
 
     def activate_venv(self):
         ''' Activates the virtual environment. This function restarts the app and switches over to using the venv interpreter. '''
-
         # Check to see if the launcher is running with the default Python interpreter or the virtual environment interpreter
-        if sys.executable != self.venv_interpreter:
-
-
+        if sys.executable != self.venv_interpreter: # Check to see of the app is using the system interpreter.
             import psutil
-
-
-            if not os.path.isdir("venv"):
-                if os.path.isdir(self.repository_name):
+            if not os.path.isdir("venv"):   # Does the virtual environment folder exist?
+                if os.path.isdir(self.repository_name):     # Check to see the repository folder exists. If so, venv has been deleted. Reload repository from stable branch.
                     subprocess.run(["rm", "-r", self.repository_name], stdout=subprocess.PIPE, text=True, check=True)
-                    if os.path.isfile("config.txt"):
+                    if os.path.isfile("config.txt"):    # Remove old config.txt as it is now outdated
                         subprocess.run(["rm", "config.txt"], stdout=subprocess.PIPE, text=True, check=True)
-
-                create_venv = subprocess.run(["virtualenv", "venv"], stdout=subprocess.PIPE, text=True, check=True)
+                create_venv = subprocess.run(["virtualenv", "venv"], stdout=subprocess.PIPE, text=True, check=True) # Create a new virtual environment
                 if create_venv.returncode:
                     print("Error: Failed to create VirtualEnv")
-
             try:
-                p = psutil.Process(os.getpid())
-                for handler in p.open_files() + p.connections():
+                p = psutil.Process(os.getpid())     # Get the current process id of this launcher
+                for handler in p.open_files() + p.connections():    # Close any open files and connections held by this process
                     os.close(handler.fd)
             except Exception as e:
                 # logging.error(e)
                 pass
-
             # Relaunch application using virtual environment interpreter
             os.execl(self.venv_interpreter, self.venv_interpreter, *sys.argv)
         else:
-            # Executes after application has restarted
+            # Executes after application has restarted. Changes path variables to point to venv interpreter.
             exec(open("venv/bin/activate_this.py").read(), {'__file__': "venv/bin/activate_this.py"})
-
-            if self.required_modules:
+            if self.required_modules:   # List of modules required by this launcher
                 print("Updating modules required by launcher")
-                for module in self.required_modules:
+                for module in self.required_modules:    # Install required modules using pip
                     proc = subprocess.run(["pip", "install", module], capture_output=True, encoding="utf-8")
                     print(proc.stdout)
-
             print("Modules currently installed:")
             installed_modules = subprocess.run(["pip", "list"], capture_output=True, encoding="utf-8")
             result = installed_modules.stdout
@@ -95,11 +83,9 @@ class Init:
             # ClaverNode = list(map(" ".join, zip(i, i)))
             print(result)
 
-
     def restart_program(self):
         """ Restarts the current program """
         import psutil
-
         try:
             p = psutil.Process(os.getpid())
             for handler in p.open_files() + p.connections():
@@ -107,21 +93,21 @@ class Init:
         except Exception as e:
             # logging.error(e)
             pass
-
-        python = sys.executable
-        os.execl(python, python, *sys.argv)
+        # Relaunch application using virtual environment interpreter
+        os.execl(self.venv_interpreter, self.venv_interpreter, *sys.argv)
 
     def load_config_file(self, config):
+        ''' Read in the contents of JSON file (config.txt) '''
         with open(config) as file:
             self.config = json.load(file)
 
     def save_config_file(self):
+        ''' Save config information in JSON format to config.txt '''
         with open('config.txt', 'w') as outfile:
             json.dump(self.config, outfile)
 
     def load_client_app(self):
         ''' Ensures that a running copy of the app has been downloaded. Rolls back any failed version. '''
-        # if not os.path.isfile("config.txt"):    # Checks to see if any version has run previously.
         if self.config is None:
             config = self.repository_name + "/src/config.txt"
             if not os.path.isfile(config):
@@ -129,65 +115,81 @@ class Init:
                 if clone_git.returncode:
                     print(f"Error: Failed to clone app from {self.repository_url}: branch={self.repository_branch}")
                     return False
-
                 # Modules must not start with a number
                 self.repository_name = "t" + str(self.clock)
-
                 # Install modules listed in requirements.txt
                 requirements_path = self.repository_name + "/requirements/requirements.txt"
                 install_requirements = subprocess.run(["pip", "install", "-r", requirements_path], stdout=subprocess.PIPE, text=True, check=True)
                 if install_requirements.returncode:
                     print("Error: Failed to load requirements.txt")
                     return False
-
                 # Set location of config.txt
                 config = self.repository_name + "/src/config.txt"
                 if not os.path.isfile(config):
                     print("Failed to locate config file")
                     return False
-
                 self.load_config_file(config)
-                self.config["dir"] = self.repository_name   # Save the name of repository to config.txt
-                self.save_config_file()
-
-        # else:
-            # self.load_config_file("config.txt")
-            # self.repository_name = self.config["dir"]
-            # ToDo: Check to see if the last run attempt was successful
-
+                self.config["app_dir"] = self.repository_name   # Save the name of repository to config.txt
         return True
+
+    def upgrade_client_app(self):
+        ''' Download the newest version of the client app and restart app. '''
+        clone_git = subprocess.run(["git", "clone", "--single-branch", "--branch", self.repository_branch, self.repository_url, "t" + str(self.clock)], stdout=subprocess.PIPE, text=True, check=True)
+        if clone_git.returncode:
+            print(f"Error: Failed to clone app from {self.repository_url}: branch={self.repository_branch}")
+            return False
+        # Modules must not start with a number
+        self.repository_name = "t" + str(self.clock)
+        # Install modules listed in requirements.txt
+        requirements_path = self.repository_name + "/requirements/requirements.txt"
+        install_requirements = subprocess.run(["pip", "install", "-r", requirements_path], stdout=subprocess.PIPE, text=True, check=True)
+        if install_requirements.returncode:
+            print("Error: Failed to load requirements.txt")
+            return False
+        self.config["previous_app_dir"] = self.config["app_dir"]
+        self.config["app_dir"] = self.repository_name
+        self.save_config_file()
+        self.restart_program()
 
     def set_exit_status(self, val, **kwargs):
         ''' Callback function passed to application module. GTK does not allow setting the exit status directly. '''
-        self.exit_status = val
+        self.action_request = val
 
     def launch_app(self):
+        ''' Dynamically loads app based on repository name. Assumes main class matches repository name. Deletes previously installed version of the app. '''
         # Import the module
         mod = importlib.import_module(f'{self.repository_name}.src.launcher')
-
         # Determine a list of names to copy to the current name space
         names = getattr(mod, '__all__', [n for n in dir(mod) if not n.startswith('_')])
-
-        # Add the name of the entry-point class into the current name space
+        # Copy the name of the entry-point class into the current name space
         g = globals()
         for name in names:
             # Look for the class that matches the repository name
             if name == self.repository_class_name:
                 entry_point = getattr(mod, name)
                 g[name] = entry_point
+        self.application = entry_point(self.set_exit_status)    # Instantiate app class
+        self.config["app_exit_status"] = self.application.run()     # Entry-point for GTK applications is run()
+        if "previous_app_dir" in self.config \
+                and not self.config["app_exit_status"] \
+                and os.path.isdir(self.config["previous_app_dir"]): # If app ran without error (exit-status == 0), check for previous version of app and delete directory
+            print("Removing previous version directory")
+            subprocess.run(["rm", "-r", self.config["previous_app_dir"]], stdout=subprocess.PIPE, text=True, check=True)
+            if not os.path.isdir(self.config["previous_app_dir"]):  # Make sure the directory was deleted
+                del self.config["previous_app_dir"]     # Remove key from the config dictionary
 
-        self.application = entry_point(self.set_exit_status)
-        self.application.run()
-
-    def evaluate_exit_status(self):
-        print(f"Exit Status: {self.exit_status}")
-        if self.exit_status is None:
+    def evaluate_action_request(self):
+        ''' Action any requests sent by the app '''
+        self.config["action_request"] = self.action_request
+        print(f"Exit Status: {self.action_request}")
+        if self.action_request is None:
             print("App failed to start")
-        elif self.exit_status == 0:
-            print("App exited successfully")
-            # ToDo: Update config with successful exit status
-        elif self.exit_status == 1:
+        elif self.action_request == 0:
+            print("No actionable requests sent")
+        elif self.action_request == 1:
             print("Request to upgrade app")
+            self.upgrade_client_app()
+
 
 if __name__ == "__main__":
     Init()
